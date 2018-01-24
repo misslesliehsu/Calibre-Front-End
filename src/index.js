c1a72e156bc0c916f63ed79a2aa972a9b9a99071
Marvin Clerge <clergemarvin@gmail.com>
2018-01-24 12:49:56 -0500

    init wed

--- src/index.js
+++ src/index.js
@@ -2,4 +2,16 @@ const store = {media: []}
 
 document.addEventListener('DOMContentLoaded', () => {
   App.init()
+
+  document.querySelector('#mp')
+  .addEventListener('click', () => {
+    document.querySelector('.grid').style.display = 'none'
+  })
+
+  document.querySelector('#br')
+  .addEventListener('click', () => {
+    document.querySelector('.grid').style.display = 'none'
+  })
+
+
 })
