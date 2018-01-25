646a40c2e89f890d1fa015bc75b2c022dd1a8a1e
eal6gf <elum1234@gmail.com>
2018-01-25 10:40:58 -0500

    delete comments

--- src/app.js
+++ src/app.js
@@ -23,6 +23,7 @@ class App {
     App.handleLikeButton();
     App.handleLogin();
     App.handleCommentSubmit();
+    App.handleCommentDelete();
     App.handleNewSearch()
     App.handleRepeat()
     App.handleAutoPlay()
@@ -266,6 +267,25 @@ class App {
       //reset value
       commentInput.value = ''
     }
+  }
+
+  static handleCommentDelete(){
+    let commentsDiv = document.getElementById('comments')
+    commentsDiv.addEventListener('click', commentDelete)
+
+    function commentDelete(e){
+      //event propagation
+      if (e.target.className === 'delete'){
+        //delete comment node
+        let commentId = parseInt(e.target.parentNode.dataset['commentId'])
+        e.target.parentNode.remove()
+        // remove comment
+        Adapter.deleteComment(commentId)
+      }
+        e.stopPropagation()
+    }
+
+
   }
 
 
